11f08e127a072e815eb3da447beebda63a489f93
Rocco Meli <r.meli@bluemail.ch>
2023-01-05 23:34:25 +0100

    nicer git icons based on nerd fonts

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -6,37 +6,39 @@ require("gitsigns").setup({
     signs = {
         add = {
             hl = "GitSignsAdd",
-            text = "+",
+            text = "",
             numhl = "GitSignsAddNr",
             linehl = "GitSignsAddLn",
         },
         change = {
             hl = "GitSignsChange",
-            text = "│",
+            text = "",
             numhl = "GitSignsChangeNr",
             linehl = "GitSignsChangeLn",
         },
         delete = {
             hl = "GitSignsDelete",
-            text = "-",
+            text = "",
             numhl = "GitSignsDeleteNr",
             linehl = "GitSignsDeleteLn",
         },
+        -- First line of file deleted
         topdelete = {
             hl = "GitSignsDelete",
-            text = "‾",
+            text = "",
             numhl = "GitSignsDeleteNr",
             linehl = "GitSignsDeleteLn",
         },
+        -- Modified lines occupying same space as deleted lines
         changedelete = {
             hl = "GitSignsChange",
-            text = "~",
+            text = "",
             numhl = "GitSignsChangeNr",
             linehl = "GitSignsChangeLn",
         },
         untracked = {
             hl = "GitSignsAdd",
-            text = "┆",
+            text = "",
             numhl = "GitSignsAddNr",
             linehl = "GitSignsAddLn",
         },
