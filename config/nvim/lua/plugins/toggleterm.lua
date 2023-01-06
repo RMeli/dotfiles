82e4bbc2935943fba0e6f147850a0bbde60976ab
Rocco Meli <r.meli@bluemail.ch>
2023-01-06 13:28:49 +0100

    always save all buffers when opening a terminal

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -4,6 +4,9 @@
 
 require("toggleterm").setup({
     open_mapping = [[<c-\>]],
+    insert_mapping = true, -- Open mapping also works in insert mode
+    on_open = function()
+        vim.cmd("wa") -- Save all buffers when opening a terminal
+    end,
     direction = "float",
 })
-
