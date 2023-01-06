a10cdbd13e2c7c875fa8db417b59f41742cc7e71
Rocco Meli <r.meli@bluemail.ch>
2023-01-06 15:59:11 +0100

    allow completion selection with tab

--- config/nvim/lua/plugins/cmp-nvim.lua
+++ config/nvim/lua/plugins/cmp-nvim.lua
@@ -78,8 +78,17 @@ cmp.setup({
       c = cmp.mapping.close(), -- Command mode
     }),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
   }),
-
   -- Use buffer source for '/' and '?'
   cmp.setup.cmdline({ "/", "?" }, {
     mapping = cmp.mapping.preset.cmdline(),
