7fe3a70ac3701c06a15a2347805d9de7dae84680
Rocco Meli <r.meli@bluemail.ch>
2023-01-07 23:39:51 +0100

    integrate lsp in tree and improve tree

--- config/nvim/lua/plugins/tree.lua
+++ config/nvim/lua/plugins/tree.lua
@@ -2,9 +2,39 @@
 -- A file explorer tree for neovim written in lua
 -- https://github.com/nvim-tree/nvim-tree.lua
 
-require("nvim-tree").setup({ filters = {
-  dotfiles = true,
-} })
+require("nvim-tree").setup({
+  filters = { dotfiles = true },
+  renderer = {
+    indent_markers = {
+      enable = true,
+    },
+    icons = {
+      git_placement = "after",
+      symlink_arrow = " ➛ ",
+      show = {
+        file = true, -- show file icons
+        folder = true, -- show folder icons
+        folder_arrow = true, -- ?
+        git = true, -- show git icons
+      },
+      glyphs = {
+        git = {
+          unstaged = "",
+          staged = "",
+          unmerged = "",
+          renamed = "",
+          untracked = "",
+          deleted = "",
+          ignored = "◌",
+        },
+      },
+    },
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+  },
+})
 
 -- [[ keymaps ]]
 
