be9e633faf1988c58a79ab614161d4654fe48273
Rocco Meli <r.meli@bluemail.ch>
2023-01-10 10:28:36 +0100

    cleanup

--- config/nvim/lua/plugins/lsp/keymaps.lua
+++ config/nvim/lua/plugins/lsp/keymaps.lua
@@ -14,6 +14,7 @@ if wk_ok then
     ["gd"] = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Go to Definition" },
     ["gI"] = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "Go to Implementation" },
     ["gl"] = { "<cmd>lua vim.lsp.buf.diagnostic()<cr>", "Diagnostic Float Window" },
+    ["gr"] = { "<cmd>lua vim.lsp.buf.references()<cr>", "Go to references" },
     -- LSP group
     ["<leader>l"] = { name = "+LSP" },
     ["<leader>lf"] = {
@@ -37,16 +38,12 @@ if wk_ok then
       "<cmd>lua vim.lsp.buf.signature_help()<cr>",
       "Signature Help",
     },
+    ["<leader>la"] = {
+      "<cmd>lua vim.lsp.buf.code_action()<cr>",
+      "Code Action",
+    },
   })
 end
 
--- keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
--- keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
 -- keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
--- keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
--- keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
--- keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
--- keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
--- keymap(bufnr, "n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
--- keymap(bufnr, "n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
 -- keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
