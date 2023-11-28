00769431679af19574cf2812e2e44a23f1e3c3eb
Rocco Meli <r.meli@bluemail.ch>
2023-11-28 23:32:45 +0100

    define formatters

--- nvchad/configs/null-ls.lua
+++ nvchad/configs/null-ls.lua
@@ -1,18 +1,17 @@
+nvchad/configs/null-ls.lua
 local null_ls = require "null-ls"
 
 local b = null_ls.builtins
 
 local sources = {
-
-  -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
-
-  -- Lua
-  b.formatting.stylua,
-
-  -- cpp
-  b.formatting.clang_format,
+  b.formatting.stylua, -- Lua
+  b.formatting.clang_format, -- C/C++
+  b.formatting.fprettify, -- Fortran
+  -- Python
+  b.formatting.black,
+  b.formatting.isort,
+  b.diagnostics.flake8,
+  b.diagnostics.mypy,
 }
 
 null_ls.setup {
